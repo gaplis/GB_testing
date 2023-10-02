450a56f466af30c4b18fa26b69575bf13ecc77ff
IlyaChistov <gaplis322@gmail.com>
2023-10-02 20:02:17 +0700

    Lection 4

---
+++ src/lections/lectionFour/main/Mockito.java
@@ -0,0 +1,14 @@
+package lections.lectionFour.main;
+import java.util.List;
+
+import static org.mockito.Mockito.*;
+
+public class Mockito {
+    public static void main(String[] args) {
+        List mockedList = mock(List.class);
+
+        mockedList.add("one");
+        System.out.println(mockedList.get(0));
+        mockedList.clear();
+    }
+}
