994d731040a20d50301e2051f9906883ddc4dbdf
IlyaChistov <gaplis322@gmail.com>
2023-10-07 23:55:33 +0700

    Lection 6

---
+++ src/lections/lectionSix/test/test_Car.py
@@ -0,0 +1,31 @@
+from unittest import TestCase
+
+from src.lections.lectionSix.main.Car import Car
+
+
+class TestCar(TestCase):
+    def setUp(self):
+        self.car = Car()
+
+
+class TestInit(TestCar):
+    def test_initial_speed(self):
+        self.assertEqual(self.car.speed, 0)
+
+    def test_initial_odometer(self):
+        self.assertEqual(self.car.odometer, 0)
+
+    def test_initial_time(self):
+        self.assertEqual(self.car.time, 0)
+
+
+class TestAccelerate(TestCar):
+    def test_accelerate_from_zero(self):
+        self.car.accelerate()
+        self.assertEqual(self.car.speed, 5)
+
+    def test_multiple_accelerates(self):
+        for _ in range(3):
+            self.car.accelerate()
+
+        self.assertEqual(self.car.speed, 15)
